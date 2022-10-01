caa8f3750827504ee6ae8aa9b70cb35af0a1ab04
Peem Auppati <104556421+PeemXD@users.noreply.github.com>
2022-10-01 22:37:43 +0700

    +insert promotion

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-import express, { json } from 'express';
-import cors from 'cors';
-// import { createConnection } from 'mysql2';
-// import { v4 as uuidv4 } from 'uuid';
+import express, { json } from "express";
+import cors from "cors";
+import { createConnection } from "mysql2";
+import { v4 as uuidv4 } from "uuid";
+import {insertPromotion} from "./promotion/insert_p.js"
 
 const app = express();
 app.use(cors());
 app.use(json());
 
-// const con = createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: '123',
-//     database: "carcare"
-//   });
-// con.connect();
+const con = createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "carcare",
+});
+con.connect();
 
+app.get("/", (req, res) => {
+  res.send("Hello Peem");
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello Peem');
-})
+app.put("/promotion", (req, res) => {
+    insertPromotion(con, req, res);
+});
 
-
-
-const port = process.env.PORT || 3307
+const port = process.env.PORT || 3307;
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-})
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
 //? `INSERT INTO users(userid, nickname, picture) VALUES("${uuid}","${nickname}", "${pic}")`
 //? `SELECT * FROM users WHERE u_id = '${uuid}' `
---
+++ promotion/insert_p.js
@@ -0,0 +1,74 @@
+function insertPromotion(con, req, res) {
+  const code = req.body?.code;
+  const name = req.body?.name;
+  const desciption = req.body?.desciption;
+  const image = req.body?.image;
+  const starttime = req.body?.starttime;
+  const endtime = req.body?.endtime;
+
+  var limitflag = req.body?.limitflag;
+  var limit_amount;
+  var limit_type;
+  if (limitflag == 1) {
+    limit_amount = req.body?.limit_amount ?? null;
+    limit_type = req.body?.limit_type ?? null;
+    if (limit_amount == null || limit_type == null) {
+      limitflag = 0;
+    }
+  }
+
+  var dayflag = req.body?.dayflag;
+  var day;
+  if (dayflag == 1) {
+    day = req.body?.day ?? null;
+    if (day == null) {
+      dayflag = 0;
+    }
+  }
+
+  var rankflag = req.body?.rankflag;
+  var rank;
+  if (rankflag == 1) {
+    rank = req.body?.rank ?? null;
+    if (rank == null) {
+      rankflag = 0;
+    }
+  }
+
+  con.connect((err) => {
+    if (err) throw err;
+    console.log("\nConnected!");
+
+    con.query(
+      `SELECT code FROM promotion WHERE code in ("${code}")`,
+      (err, result) => {
+        if (result[0] == undefined) {
+          //! result == []
+          var sql1 = `INSERT INTO promotion(code, name, desciption, image, starttime, endtime, limitflag, limit_amount, limit_type, dayflag, rankflag, rank) 
+                    VALUES("${code}", "${name}", "${desciption}", "${image}", "${starttime}", "${endtime}", "${limitflag}", "${limit_amount}", "${limit_type}", "${dayflag}", "${rankflag}", "${rank}")`;
+          con.query(sql1, (err, result) => {
+            if (err) throw err;
+            console.log("insert promotion success");
+
+            if (dayflag == true) {
+              var sql2 = `INSERT INTO promotion_by_day(code, day) 
+                                        VALUES("${code}", "${day}")`;
+              con.query(sql2, (err, result) => {
+                if (err) throw err;
+                console.log("insert promotion_by_day success");
+              });
+            } else {
+              console.log("no promotion_by_day");
+            }
+          });
+        } else {
+          console.log(`code ${result[0].code} is duplicate`);
+          res.status(404).send();
+          return 0;
+        }
+      }
+    );
+  });
+}
+
+export {insertPromotion};
